b5a90de96239a0273d1e56c0a73c4edec21de6f8
Hafizul Islam <hafizul2004official@gmail.com>
2026-08-13 21:43:34 +0600

    Data Add In Backend

--- My_App/src/Components/Users.jsx
+++ My_App/src/Components/Users.jsx
@@ -1,12 +1,47 @@
-import { use } from "react";
+import { use, useState } from "react";
 
-const Users = ({Data}) => {
-    const User=use(Data)
+const Users = ({ Data }) => {
+    const User = use(Data)
+    const [users,setUsers]=useState(User)
     console.log(User);
-    
+
+    const submitHendaler = (e) => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+
+        const newUser = { name, email }
+
+        console.log("Submitted", name, email);
+        fetch("http://localhost:3000/user", {
+            method: "POST",
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log("After Post", data);
+
+            })
+        e.target.reset()
+    }
+
     return (
         <div>
             Length:{User.length}
+            {
+                /* {
+                    User.map(UserData=><p key={UserData.id}>Name:{UserData.name}<br/> Emil:{UserData.email}</p>)
+                } */
+            }
+
+            <form onSubmit={submitHendaler}>
+                <input type="text" name="name" /> <br />
+                <input type="email" name="email" /> <br />
+                <button>Add User</button>
+            </form>
         </div>
     );
 };
